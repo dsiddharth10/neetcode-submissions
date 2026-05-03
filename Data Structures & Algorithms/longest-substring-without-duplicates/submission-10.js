1a1ccd913273c55dd2d1fd862ebcb2142a20c653
Siddharth Dixit <Siddharth.dixit10@gmail.com>
2026-05-04 00:22:08 +0530

    Add: longest-substring-without-duplicates - submission-10

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-10.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        const charSet = new Set();
+        let maxLen = 0;
+
+        let l = 0;
+        let r = 0;
+
+        if (s.length === 0) return 0;
+        if (s.length === 1) return 1;
+
+        while (r < s.length) {
+            if (charSet.has(s[r])) {
+                maxLen = Math.max(maxLen, r - l);
+                while(charSet.has(s[r])) {
+                    charSet.delete(s[l]);
+                    l++;
+                }
+                charSet.add(s[r]);
+                r++;
+            } else {
+                charSet.add(s[r]);
+                r++;
+            }
+            maxLen = Math.max(maxLen, r - l);
+        }
+
+        return maxLen;
+    }
+}
+
+            
